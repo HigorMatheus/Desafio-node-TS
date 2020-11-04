60d9d289dac5d582b855d23299a4064696db835c
Higor Matheus O Vieira <hmoliveira83@gmail.com>
2020-11-04 11:14:23 -0300

    add desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,6 +5,11 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface CreateTransactinDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -14,15 +19,76 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    const Transactions = this.transactions;
+    return Transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    // const entradas = this.transactions.map(transaction => {
+    //   const entrada = transaction.type === 'income';
+    //   const valor = transaction.value;
+    //   return { entrada, valor };
+    // });
+    // const saidas = this.transactions.map(transaction => {
+    //   const saida = transaction.type === 'outcome';
+    //   const valor = transaction.value;
+    //   return { saida, valor };
+    // });
+
+    // const entradasTotal = entradas.map(entrada => {
+    //   const total = entrada.valor;
+    //   return total;
+    // });
+
+    // const saidasTotal = saidas.map(saida => {
+    //   const total = saida.valor;
+    //   return total;
+    // });
+    // const totalSaidas = saidasTotal[0] + saidasTotal[0];
+    // const totalEntradas = entradasTotal[0] + entradasTotal[0];
+    // const balanceTotal = totalEntradas - totalSaidas;
+    // const balance = {
+    //   income: totalEntradas,
+    //   outcome: totalSaidas,
+    //   total: balanceTotal,
+    // };
+
+    const income = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .map(transactions => transactions.value)
+      .reduce((acc, value) => acc + value, 0);
+    const outcome = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .map(transactions => transactions.value)
+      .reduce((acc, value) => acc + value, 0);
+
+    const total = income - outcome;
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactinDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+    const income = this.transactions
+      .filter(transactio => transactio.type === 'income')
+      .map(transactions => transactions.value)
+      .reduce((acc, valor) => acc + valor, 0);
+    const outcome = this.transactions
+      .filter(transactio => transactio.type === 'outcome')
+      .map(transactions => transactions.value)
+      .reduce((acc, valor) => acc + valor, 0);
+
+    const total = income - outcome;
+
+    if (type === 'outcome' && total < value) {
+      throw new Error('transfer does not accept limit reached');
+    }
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,20 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ value, title, type }: CreateTransaction): Transaction {
+    // if ((type !== 'income') | (type !== 'outcome')) {
+    //   throw Error('operacao invalida ');
+    // }
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction type is not available');
+    }
+    const transactionCreate = this.transactionsRepository.create({
+      value,
+      title,
+      type,
+    });
+
+    return transactionCreate;
   }
 }
 
